4d7dda050397aa060fcc2f19732f0599a9e617a2
Merryn Tawhai <mhow008@bn411727.uoa.auckland.ac.nz>
2020-11-25 10:32:09 +1300

    uncommenting some lines

--- src/lib/geometry.f90
+++ src/lib/geometry.f90
@@ -553,7 +553,7 @@ contains
     call element_connectivity_1d
     call evaluate_ordering
 
-    elem_ordrs(no_type,:) = 1 ! 0 for respiratory, 1 for conducting
+!    elem_ordrs(no_type,:) = 1 ! 0 for respiratory, 1 for conducting
     
     call enter_exit(sub_name,2)
 
